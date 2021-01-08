e3e6f2e2199e9802ebd312da61031cfe87a02fbf
morbidgus <madridgusatavo23@gmail.com>
2021-01-08 20:30:03 -0300

    adding sing in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home/Home';
+import Shop from './pages/shop/Shop';
+import SignInSignUp from './pages/signin-signup/signin-signup';
+import Header from './components/header/header'
 
 function App() {
   return (
     <div>
-        <Home />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInSignUp} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/signin-signup/signin-signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './signin-signup.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+const SignInSignUp = () =>(
+  <div className="signin-signup">
+    <SignIn />
+  </div>
+)
+
+export default SignInSignUp;
